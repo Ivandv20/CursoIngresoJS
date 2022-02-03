6dc5571650d811ad758ada3d386471e1dbbf916d
Ivandv20 <ivanalejo34@gmail.com>
2022-02-03 16:06:16 -0300

    a

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -4,6 +4,22 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
-{
-	alert("ok");
+{  
+
+	 var dividendo
+	 dividendo = txtIdNumeroDividendo.value;
+    dividendo = parseInt(dividendo);
+
+    var divisor 
+
+	divisor =  txtIdNumeroDivisor.value;
+	divisor = parseInt(divisor);
+
+	var resultado
+
+	resultado = dividendo % divisor;
+
+
+
+	alert("El resto es  " + resultado);
 }
